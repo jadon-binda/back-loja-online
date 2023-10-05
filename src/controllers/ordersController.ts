d74ab9c346169b4c3a93a7bfa487aae90eced0ea
JB <jadon.binda@gmail.com>
2023-10-05 01:19:23 -0300

    feat: criação da declaração das funções e todas as rotas do controlador pedidos

---
+++ src/controllers/ordersController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const registerOrder = async (req: Request, res: Response) => {
+
+}
+
+export const listOrders = async (req: Request, res: Response) => {
+
+}
+
+export const detailOrder = async (req: Request, res: Response) => {
+
+}
+
+export const updateOrder = async (req: Request, res: Response) => {
+
+}
+
+export const deleteOrder = async (req: Request, res: Response) => {
+
+}
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
 import { deleteProduct, detailProduct, listProducts, registerProduct, updateProduct } from './controllers/productsController'
+import { deleteOrder, detailOrder, listOrders, registerOrder, updateOrder } from './controllers/ordersController'
 const router = Router()
 
-router.get('/products', listProducts)
+router.get('/product', listProducts)
 router.get('/product/:id', detailProduct)
 router.post('/product', registerProduct)
 router.put('/product/:id', updateProduct)
 router.delete('/product/:id', deleteProduct)
+
+router.get('/order', listOrders)
+router.get('/order/:id', detailOrder)
+router.post('/order', registerOrder)
+router.put('/order/:id', updateOrder)
+router.delete('/order/:id', deleteOrder)
 
 export default router
