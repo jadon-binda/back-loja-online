7767d35424b68353eef884c655ea4fc5f286eaa6
JB <jadon.binda@gmail.com>
2023-10-05 00:40:04 -0300

    feat: criação do endpoint para atualizar um produto

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -33,7 +33,7 @@ export const registerProduct = async (req: Request, res: Response) => {
       return res.status(400).json({ mensagem: 'Informe os campos obrigatórios: nome, preço e quantidade.' })
     }
 
-    await prisma.product.create({
+    const newProduct = await prisma.product.create({
       data: {
         name,
         price: Number(price),
@@ -43,12 +43,40 @@ export const registerProduct = async (req: Request, res: Response) => {
       }
     })
 
-    return res.status(201).json({ mensagem: 'Produto cadastrado com sucesso.' })
+    return res.status(201).json(newProduct)
   } catch (error) {
     return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
   }
 }
 
-// atualizar um produto
+export const updateProduct = async (req: Request, res: Response) => {
+  const { name, price, quantity, description, imageURL } = req.body
+  const { id } = req.params
+
+  try {
+    const product = await prisma.product.findUnique({
+      where: { id: id },
+    })
+
+    if (!product) {
+      return res.status(404).json({ mensagem: 'Produto não encontrado.' })
+    }
+
+    await prisma.product.update({
+      where: { id: id },
+      data: {
+        name,
+        price: Number(price),
+        quantity: Number(quantity),
+        description,
+        imageURL
+      }
+    })
+
+    return res.json({ mensagem: 'Produto atualizado com sucesso.' })
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
+  }
+}
 
 // excluir um produto
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { detailProduct, listProducts, registerProduct } from './controllers/productsController'
+import { detailProduct, listProducts, registerProduct, updateProduct } from './controllers/productsController'
 const router = Router()
 
 router.get('/products', listProducts)
 router.get('/product/:id', detailProduct)
 router.post('/product', registerProduct)
+router.put('/product/:id', updateProduct)
 
 export default router
