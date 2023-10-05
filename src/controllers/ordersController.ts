3a7fd3aa19cf42724d14325fbbe83a0b05aac7b1
JB <jadon.binda@gmail.com>
2023-10-05 01:31:38 -0300

    feat: implementação dos endpoints de cadastrar um pedido e listar todos os pedidos.

--- src/controllers/ordersController.ts
+++ src/controllers/ordersController.ts
@@ -3,11 +3,29 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export const registerOrder = async (req: Request, res: Response) => {
+  const { amount } = req.body
 
+  try {
+    const order = await prisma.order.create({
+      data: {
+        amount: Number(amount),
+        //status: false,
+      }
+    })
+
+    return res.status(201).json(order)
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
+  }
 }
 
 export const listOrders = async (req: Request, res: Response) => {
-
+  try {
+    const orders = await prisma.order.findMany()
+    return res.json(orders)
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
+  }
 }
 
 export const detailOrder = async (req: Request, res: Response) => {
